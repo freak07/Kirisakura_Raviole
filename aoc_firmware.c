54525fa1fc3652914c3e169c113deb166390debd
Craig Dooley <dooleyc@google.com>
2020-03-02 10:48:55 -0800

    Validate the firmware method signature table
    During firmware validation, make sure the method signatures between
    the kernel modules and the firmware are compatible.
    
    Bug: 143238449
    Signed-off-by: Craig Dooley <dooleyc@google.com>
    Change-Id: Idd5c62ca2e119a07d3fdbd51dd6ed52df787c4e9

--- aoc_firmware.c
+++ aoc_firmware.c
@@ -8,6 +8,7 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
+#define pr_fmt(fmt) "aoc-fw: " fmt
 
 #include <linux/slab.h>
 #include <linux/string.h>
@@ -16,6 +17,7 @@
 
 #include "aoc.h"
 #include "aoc_firmware.h"
+#include "aoc-interface.h"
 
 struct aoc_superbin_header {
 	u32 magic;
@@ -26,6 +28,8 @@ struct aoc_superbin_header {
 	u32 bootloader_high;
 	u32 bootloader_offset;
 	u32 bootloader_size;
+	u32 uuid_table_offset;
+	u32 uuid_table_size;
 	u32 section_table_offset;
 	u32 section_table_entry_size;
 	u32 sram_offset;
@@ -38,20 +42,44 @@ struct aoc_superbin_header {
 	u32 crc32;
 } __packed;
 
+static bool region_is_in_firmware(size_t start, size_t length,
+				  const struct firmware *fw)
+{
+	return ((start + length) < fw->size);
+}
+
 bool _aoc_fw_is_valid(const struct firmware *fw)
 {
 	struct aoc_superbin_header *header;
 	u32 ipc_offset, bootloader_offset;
+	u32 uuid_offset, uuid_size;
 
 	if (!fw || !fw->data)
 		return false;
 
-	if (fw->size < sizeof(*header))
+	if (!region_is_in_firmware(0, sizeof(*header), fw))
 		return false;
 
 	header = (struct aoc_superbin_header *)fw->data;
 	if (le32_to_cpu(header->magic) != 0xaabbccdd)
 		return false;
+
+	/* Validate that the AoC firmware recognizes the messages known at
+	 * compile time
+	 */
+
+	uuid_offset = le32_to_cpu(header->uuid_table_offset);
+	uuid_size = le32_to_cpu(header->uuid_table_size);
+
+	if (!region_is_in_firmware(uuid_offset, uuid_size, fw)) {
+		pr_err("invalid method signature region\n");
+		return false;
+	}
+
+	if (AocInterfaceCheck(fw->data + uuid_offset, uuid_size) != 0) {
+		pr_err("failed to validate method signature table\n");
+		return false;
+	}
 
 	ipc_offset = _aoc_fw_bootloader_offset(fw);
 	bootloader_offset = _aoc_fw_bootloader_offset(fw);
@@ -59,7 +87,8 @@ bool _aoc_fw_is_valid(const struct firmware *fw)
 	/* The bootloader resides within the FW image, so make sure
 	 * that value makes sense
 	 */
-	if (bootloader_offset > fw->size)
+	if (!region_is_in_firmware(bootloader_offset,
+				   le32_to_cpu(header->bootloader_size), fw))
 		return false;
 
 	return true;
