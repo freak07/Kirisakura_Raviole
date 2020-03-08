5b9b60a6a463bb1d07f7a7f3ba8596b2695024d4
Al Viro <viro@zeniv.linux.org.uk>
2020-03-08 09:16:37 -0400

    coredump: don't bother with do_truncate()
    have dump_skip() just remember how much needs to be skipped,
    leave actual seeks/writing zeroes to the next dump_emit()
    or the end of coredump output, whichever comes first.
    And instead of playing with do_truncate() in the end, just
    write one NUL at the end of the last gap (if any).
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit d0f1088b31db2d03497a74ca67755df5515f8ff4)
    (cherry picked from commit d78378fb0527fbfcbabd2f51f4ddc4b1e5563703)

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -2278,8 +2278,7 @@ static int elf_core_dump(struct coredump_params *cprm)
 		goto end_coredump;
 
 	/* Align to page */
-	if (!dump_skip(cprm, dataoff - cprm->pos))
-		goto end_coredump;
+	dump_skip_to(cprm, dataoff);
 
 	for (i = 0; i < vma_count; i++) {
 		struct core_vma_metadata *meta = vma_meta + i;
@@ -2287,7 +2286,6 @@ static int elf_core_dump(struct coredump_params *cprm)
 		if (!dump_user_range(cprm, meta->start, meta->dump_size))
 			goto end_coredump;
 	}
-	dump_truncate(cprm);
 
 	if (!elf_core_write_extra_data(cprm))
 		goto end_coredump;
