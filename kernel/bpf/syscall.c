7588c8bb1b823d15b717e91c4fc6def2477157b5
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-18 19:35:45 +0900

    bpf: avoid dynamic memory allocation for small value buffers
    Most info buffers are rather small and can fit on the stack,
    eliminating the need to allocate them dynamically.
    
    Reserve small stack buffers for this purpose to avoid the overhead of
    dynamic memory allocation.
    
    Co-authored-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    (cherry picked from commit 925b357088d9b4f466a52fdfa8fab374cc23b221)
    (cherry picked from commit 0022471cfc93463e6e94e2e371b55db071108ead)
    (cherry picked from commit 96b82940ad4fe51ec84501cf302bb3f8b3d57e15)
    (cherry picked from commit 64ca295c93f22c5783e3e6abbfd4ceb0c94d63bc)

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1010,6 +1010,7 @@ static int map_lookup_elem(union bpf_attr *attr)
 	int ufd = attr->map_fd;
 	struct bpf_map *map;
 	void *key, *value;
+	char value_onstack[32];
 	u32 value_size;
 	struct fd f;
 	int err;
@@ -1043,10 +1044,14 @@ static int map_lookup_elem(union bpf_attr *attr)
 
 	value_size = bpf_map_value_size(map);
 
-	err = -ENOMEM;
-	value = kmalloc(value_size, GFP_USER | __GFP_NOWARN);
-	if (!value)
-		goto free_key;
+	if (likely(value_size <= sizeof(value_onstack))) {
+		value = value_onstack;
+	} else {
+		err = -ENOMEM;
+		value = kmalloc(value_size, GFP_USER | __GFP_NOWARN);
+		if (!value)
+			goto free_key;
+	}
 
 	err = bpf_map_copy_value(map, key, value, attr->flags);
 	if (err)
@@ -1059,7 +1064,8 @@ static int map_lookup_elem(union bpf_attr *attr)
 	err = 0;
 
 free_value:
-	kfree(value);
+	if (unlikely(value != value_onstack))
+		kfree(value);
 free_key:
 	kfree(key);
 err_put:
@@ -1077,6 +1083,7 @@ static int map_update_elem(union bpf_attr *attr)
 	int ufd = attr->map_fd;
 	struct bpf_map *map;
 	void *key, *value;
+	char value_onstack[16];
 	u32 value_size;
 	struct fd f;
 	int err;
@@ -1113,10 +1120,14 @@ static int map_update_elem(union bpf_attr *attr)
 	else
 		value_size = map->value_size;
 
-	err = -ENOMEM;
-	value = kmalloc(value_size, GFP_USER | __GFP_NOWARN);
-	if (!value)
-		goto free_key;
+	if (likely(value_size <= sizeof(value_onstack))) {
+		value = value_onstack;
+	} else {
+		err = -ENOMEM;
+		value = kmalloc(value_size, GFP_USER | __GFP_NOWARN);
+		if (!value)
+			goto free_key;
+	}
 
 	err = -EFAULT;
 	if (copy_from_user(value, uvalue, value_size) != 0)
@@ -1125,7 +1136,8 @@ static int map_update_elem(union bpf_attr *attr)
 	err = bpf_map_update_value(map, f, key, value, attr->flags);
 
 free_value:
-	kfree(value);
+	if (unlikely(value != value_onstack))
+		kfree(value);
 free_key:
 	kfree(key);
 err_put:
@@ -1195,6 +1207,7 @@ static int map_get_next_key(union bpf_attr *attr)
 	int ufd = attr->map_fd;
 	struct bpf_map *map;
 	void *key, *next_key;
+	char next_key_onstack[48];
 	struct fd f;
 	int err;
 
@@ -1220,10 +1233,14 @@ static int map_get_next_key(union bpf_attr *attr)
 		key = NULL;
 	}
 
-	err = -ENOMEM;
-	next_key = kmalloc(map->key_size, GFP_USER);
-	if (!next_key)
-		goto free_key;
+	if (likely(map->key_size <= sizeof(next_key_onstack))) {
+		next_key = next_key_onstack;
+	} else {
+		err = -ENOMEM;
+		next_key = kmalloc(map->key_size, GFP_USER);
+		if (!next_key)
+			goto free_key;
+	}
 
 	if (bpf_map_is_dev_bound(map)) {
 		err = bpf_map_offload_get_next_key(map, key, next_key);
@@ -1244,7 +1261,8 @@ out:
 	err = 0;
 
 free_next_key:
-	kfree(next_key);
+	if (unlikely(next_key != next_key_onstack))
+		kfree(next_key);
 free_key:
 	kfree(key);
 err_put:
