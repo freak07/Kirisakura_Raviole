freak07/Kirisakura_Raviole

commit cf8c8f42dc8c719a890cdee67a76811423614d00

Opt in to power management to enable the sysmmu
Signed-off-by: Craig Dooley <dooleyc@google.com>
Change-Id: Ib27d15f37ca1784f2fe3266ce748c50a948ab6ba

aoc.c
@@ -33,6 +33,7 @@
 #include <linux/of_address.h>
 #include <linux/platform_data/sscoredump.h>
 #include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
 #include <linux/slab.h>
 #include <linux/timer.h>
 #include <linux/uaccess.h>
@@ -1085,29 +1086,35 @@ static void aoc_configure_sysmmu(struct aoc_prvdata *p)
 {
 #ifndef AOC_JUNO
 	struct iommu_domain *domain = p->domain;
+	struct device *dev = p->dev;
 
 	iommu_set_fault_handler(domain, aoc_iommu_fault_handler, NULL);
 
 	/* Map in the AoC carveout */
-	iommu_map(domain, 0x98000000, p->dram_resource.start, p->dram_size,
-		  IOMMU_READ | IOMMU_WRITE);
+	if (iommu_map(domain, 0x98000000, p->dram_resource.start, p->dram_size,
+		      IOMMU_READ | IOMMU_WRITE))
+		dev_err(dev, "mapping carveout failed\n");
 
 	/* Use a 1MB mapping instead of individual mailboxes for now */
 	/* TODO: Turn the mailbox address ranges into dtb entries */
-	iommu_map(domain, 0x9A000000, 0x17600000, SZ_1M,
-		  IOMMU_READ | IOMMU_WRITE);
+	if (iommu_map(domain, 0x9A000000, 0x17600000, SZ_1M,
+		      IOMMU_READ | IOMMU_WRITE))
+		dev_err(dev, "mapping mailboxes failed\n");
 
 	/* Map in GSA mailbox */
-	iommu_map(domain, 0x9A100000, 0x17C00000, SZ_1M,
-		  IOMMU_READ | IOMMU_WRITE);
+	if (iommu_map(domain, 0x9A100000, 0x17C00000, SZ_1M,
+		      IOMMU_READ | IOMMU_WRITE))
+		dev_err(dev, "mapping gsa mailbox failed\n");
 
 	/* Map in USB for low power audio */
-	iommu_map(domain, 0x9A200000, 0x11100000, SZ_1M,
-		  IOMMU_READ | IOMMU_WRITE);
+	if (iommu_map(domain, 0x9A200000, 0x11100000, SZ_1M,
+		      IOMMU_READ | IOMMU_WRITE))
+		dev_err(dev, "mapping usb failed\n");
 
 	/* Map in modem registers */
-	iommu_map(domain, 0x9A300000, 0x40000000, SZ_1M,
-		  IOMMU_READ | IOMMU_WRITE);
+	if (iommu_map(domain, 0x9A300000, 0x40000000, SZ_1M,
+		      IOMMU_READ | IOMMU_WRITE))
+		dev_err(dev, "mapping modem failed\n");
 #endif
 }
 
@@ -1566,6 +1573,9 @@ static int aoc_platform_probe(struct platform_device *pdev)
 	}
 
 #ifndef AOC_JUNO
+	pm_runtime_set_active(dev);
+	pm_runtime_enable(dev);
+
 	prvdata->domain = iommu_get_domain_for_dev(dev);
 	if (!prvdata->domain) {
 		pr_err("failed to find iommu domain\n");

commit 1ff267f4147c0a0eed316748453b73a6da003806

Leave runtime power management disabled
Previously, we enabled runtime power management (rpm) like so:

pm_runtime_set_active(dev);
pm_runtime_enable(dev);

The call to pm_runtime_set_active() did set AoC to RPM_ACTIVE. However,
the subsequent call to pm_runtime_enable() enabled rpm and as a result,
the framework started routinely checking if AoC is idle. It then
erroneously found that AoC is idle and suspended it including suppliers
like SysMMU.

Let's avoid this problem by setting the state of AoC to active and
leaving rpm disabled.

In aoc_watchdog_restart(), set AoC's state to suspended, then use
flush_workqueue(pm_wq) to wait for asynchronous suspend operations to
complete before setting the state back to active.

Previously, SysMMU was actually suspended most of the time, because the
framework determined that AoC is in idle state.

Bug: 173760041
Fixes: cf8c8f4 ("Opt in to power management to enable the sysmmu")
Signed-off-by: Daniel Mentz <danielmentz@google.com>
Change-Id: I29607e6e4b92881d0deea2efc8462cd8d9bc2be1

aoc.c
@@ -790,12 +790,14 @@ static int aoc_watchdog_restart(struct aoc_prvdata *prvdata)
 	writel(prvdata->aoc_s2mpu_saved_value, prvdata->aoc_s2mpu_virt + AOC_S2MPU_CTRL0);
 
 	/* Restore SysMMU */
-	rc = pm_runtime_suspend(prvdata->dev);
+	rc = pm_runtime_set_suspended(prvdata->dev);
 	if (rc < 0) {
 		dev_err(prvdata->dev, "sysmmu restore failed: pm_runtime_suspend rc = %d\n", rc);
 		return rc;
 	}
-	rc = pm_runtime_resume(prvdata->dev);
+	/* Wait for suppliers including SysMMU to suspend */
+	flush_workqueue(pm_wq);
+	rc = pm_runtime_set_active(prvdata->dev);
 	if (rc < 0) {
 		dev_err(prvdata->dev, "sysmmu restore failed: pm_runtime_resume rc = %d\n", rc);
 		return rc;
@@ -1816,7 +1818,6 @@ static int aoc_platform_probe(struct platform_device *pdev)
 	prvdata->aoc_s2mpu_saved_value = ioread32(prvdata->aoc_s2mpu_virt + AOC_S2MPU_CTRL0);
 
 	pm_runtime_set_active(dev);
-	pm_runtime_enable(dev);
 
 	prvdata->domain = iommu_get_domain_for_dev(dev);
 	if (!prvdata->domain) {