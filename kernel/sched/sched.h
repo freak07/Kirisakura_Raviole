004a0922269ea8bbfa7f54af16f62c1367edc1e5
Viresh Kumar <viresh.kumar@linaro.org>
2020-12-08 09:46:55 +0530

    sched/core: Move schedutil_cpu_util() to core.c
    There is nothing schedutil specific in schedutil_cpu_util(), move it to
    core.c and define it only for CONFIG_SMP.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://lkml.kernel.org/r/c921a362c78e1324f8ebc5aaa12f53e309c5a8a2.1607400596.git.viresh.kumar@linaro.org
    (cherry picked from commit 7d6a905f3dd62c4502cdd772c71319de4058ec89)
    (cherry picked from commit 86e76a49edbaf2a9ea7893317ad3474dbc512f96)
    (cherry picked from commit c7c17edc8c167054b5e88c589cc37e13d0cf893b)

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2891,7 +2891,6 @@ static inline unsigned long capacity_orig_of(int cpu)
 {
 	return cpu_rq(cpu)->cpu_capacity_orig;
 }
-#endif
 
 /*
  * Returns inverted capacity if the CPU is in capacity inversion state.
@@ -2927,8 +2926,6 @@ enum schedutil_type {
 	ENERGY_UTIL,
 };
 
-#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL
-
 unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 				 unsigned long max, enum schedutil_type type,
 				 struct task_struct *p);
@@ -2959,14 +2956,7 @@ static inline unsigned long cpu_util_rt(struct rq *rq)
 {
 	return READ_ONCE(rq->avg_rt.util_avg);
 }
-#else /* CONFIG_CPU_FREQ_GOV_SCHEDUTIL */
-static inline unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
-				 unsigned long max, enum schedutil_type type,
-				 struct task_struct *p)
-{
-	return 0;
-}
-#endif /* CONFIG_CPU_FREQ_GOV_SCHEDUTIL */
+#endif
 
 #ifdef CONFIG_HAVE_SCHED_AVG_IRQ
 static inline unsigned long cpu_util_irq(struct rq *rq)
