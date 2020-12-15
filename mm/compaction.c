3709894c389b4ba27ec74f6af2b670d0dcf5c088
Hui Su <sh_def@163.com>
2020-12-14 19:12:42 -0800

    mm/compaction: move compaction_suitable's comment to right place
    Since commit 837d026d560c ("mm/compaction: more trace to understand
    when/why compaction start/finish"), the comment place is not suitable.
    
    So move compaction_suitable's comment to right place.
    
    Link: https://lkml.kernel.org/r/20201116144121.GA385717@rlk
    Signed-off-by: Hui Su <sh_def@163.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 2b1a20c3afbc0279cbe57b0f19748081eba0881b)

--- mm/compaction.c
+++ mm/compaction.c
@@ -2154,13 +2154,6 @@ static enum compact_result compact_finished(struct compact_control *cc)
 	return ret;
 }
 
-/*
- * compaction_suitable: Is this suitable to run compaction on this zone now?
- * Returns
- *   COMPACT_SKIPPED  - If there are too few free pages for compaction
- *   COMPACT_SUCCESS  - If the allocation would succeed without compaction
- *   COMPACT_CONTINUE - If compaction should run now
- */
 static enum compact_result __compaction_suitable(struct zone *zone, int order,
 					unsigned int alloc_flags,
 					int highest_zoneidx,
@@ -2204,6 +2197,13 @@ static enum compact_result __compaction_suitable(struct zone *zone, int order,
 	return COMPACT_CONTINUE;
 }
 
+/*
+ * compaction_suitable: Is this suitable to run compaction on this zone now?
+ * Returns
+ *   COMPACT_SKIPPED  - If there are too few free pages for compaction
+ *   COMPACT_SUCCESS  - If the allocation would succeed without compaction
+ *   COMPACT_CONTINUE - If compaction should run now
+ */
 enum compact_result compaction_suitable(struct zone *zone, int order,
 					unsigned int alloc_flags,
 					int highest_zoneidx)
