cecf4a96aceefb72e4ca3fc0177a43dc02c6f221
Siqi Lin <siqilin@google.com>
2021-02-15 20:45:56 -0800

    Control AoC reset from AP
    Previously, AoC firmware was in charge of resetting AoC by writing
    the PCU_WATCHDOG_CONTROL register to generate an interrupt to
    the APM. This reset can fail if AoC firmware crashes or hangs in
    a really bad state, and it doesn't work if AoC crashes while AP
    is in suspend. With this patch, AoC will generate an interrupt to
    the AP, and the AP will ultimately reset AoC after collecting a
    subsystem ramdump. It will also allow AP to reset AoC in response
    to AoC hardware watchdog without any involvement from AoC firmware.
    
    Bug: 178636702
    Bug: 179696125
    Change-Id: I3379039e6852f61f0b6c999b03cbe354ac8efb86
    Signed-off-by: Siqi Lin <siqilin@google.com>

--- aoc.c
+++ aoc.c
@@ -43,6 +43,7 @@
 #include <linux/wait.h>
 #include <linux/workqueue.h>
 #include <soc/google/acpm_ipc_ctrl.h>
+#include <soc/google/exynos-cpupm.h>
 
 #if IS_ENABLED(CONFIG_EXYNOS_ITMON)
 #include <soc/google/exynos-itmon.h>
@@ -59,8 +60,6 @@
 
 #define MAX_FIRMWARE_LENGTH 128
 #define AOC_S2MPU_CTRL0 0x0
-#define AOC_PCU_RESET_CONTROL 0x0
-#define AOC_PCU_RESET_CONTROL_RESET_VALUE 0x0
 
 #define AOC_MAX_MINOR (1U)
 #define AOC_MBOX_CHANNELS 16
@@ -406,6 +405,20 @@ static void aoc_req_assert(struct aoc_prvdata *p, bool assert)
 	iowrite32(!!assert, p->aoc_req_virt);
 }
 
+static int aoc_req_wait(struct aoc_prvdata *p, bool assert)
+{
+	unsigned long aoc_req_timeout;
+
+	aoc_req_timeout = jiffies + (2 * HZ);
+	while (time_before(jiffies, aoc_req_timeout)) {
+		if (!!readl(p->aoc_req_virt + 0x40) == !!assert)
+			return 0;
+		msleep(100);
+	}
+
+	return -ETIMEDOUT;
+}
+
 extern int gs_chipid_get_ap_hw_tune_array(const u8 **array);
 
 static bool aoc_sram_was_repaired(struct aoc_prvdata *prvdata)
@@ -629,9 +642,14 @@ static void aoc_fw_callback(const struct firmware *fw, void *ctx)
 
 	write_reset_trampoline(AOC_BINARY_LOAD_ADDRESS + bootloader_offset);
 
-	aoc_a32_reset();
-
+	dev_info(dev, "disabling SICD for 2 sec for aoc boot\n");
+	disable_power_mode(0, POWERMODE_TYPE_SYSTEM);
 	prvdata->ipc_base = aoc_dram_translate(prvdata, ipc_offset);
+	aoc_a32_reset();
+	msleep(2000);
+	dev_info(dev, "re-enabling SICD\n");
+	enable_power_mode(0, POWERMODE_TYPE_SYSTEM);
+
 free_fw:
 	release_firmware(fw);
 }
@@ -1097,7 +1115,7 @@ static bool aoc_fpga_reset(struct aoc_prvdata *prvdata)
 static bool aoc_a32_reset(void)
 {
 	u32 pcu_value;
-	u32 *pcu = aoc_sram_translate(AOC_PCU_BASE);
+	void __iomem *pcu = aoc_sram_translate(AOC_PCU_BASE);
 
 	if (!pcu)
 		return false;
@@ -1113,9 +1131,34 @@ static bool aoc_a32_reset(void)
 static int aoc_watchdog_restart(struct aoc_prvdata *prvdata)
 {
 	struct device *dev = prvdata->dev;
+	/* 4100 * 0.244 us * 100 = 100 ms */
+	const int aoc_watchdog_value_ssr = 4100 * 100;
 	const int aoc_reset_timeout_ms = 1000;
+	const u32 aoc_watchdog_control_ssr = 0x2F;
 	int rc, i;
-	u32 *pcu;
+	void __iomem *pcu;
+
+	pcu = aoc_sram_translate(AOC_PCU_BASE);
+	if (!pcu)
+		return -ENODEV;
+
+	dev_info(prvdata->dev, "asserting aoc_req\n");
+	aoc_req_assert(prvdata, true);
+	rc = aoc_req_wait(prvdata, true);
+	if (rc) {
+		dev_err(prvdata->dev, "timed out waiting for aoc_ack\n");
+		return rc;
+	}
+
+	dev_info(prvdata->dev, "resetting aoc\n");
+	writel(AOC_PCU_WATCHDOG_KEY_UNLOCK, pcu + AOC_PCU_WATCHDOG_KEY_OFFSET);
+	if ((readl(pcu + AOC_PCU_WATCHDOG_CONTROL_OFFSET) &
+			AOC_PCU_WATCHDOG_CONTROL_KEY_ENABLED_MASK) == 0) {
+		dev_err(prvdata->dev, "unlock aoc watchdog failed\n");
+		return -EINVAL;
+	}
+	writel(aoc_watchdog_value_ssr, pcu + AOC_PCU_WATCHDOG_VALUE_OFFSET);
+	writel(aoc_watchdog_control_ssr, pcu + AOC_PCU_WATCHDOG_CONTROL_OFFSET);
 
 	dev_info(prvdata->dev, "waiting for aoc reset to finish\n");
 	if (wait_event_timeout(prvdata->aoc_reset_wait_queue, prvdata->aoc_reset_done,
@@ -1126,11 +1169,7 @@ static int aoc_watchdog_restart(struct aoc_prvdata *prvdata)
 	dev_info(prvdata->dev, "aoc reset finished\n");
 	prvdata->aoc_reset_done = false;
 
-	pcu = aoc_sram_translate(AOC_PCU_BASE);
-	if (!pcu)
-		return -ENODEV;
-
-	if (readl(pcu + AOC_PCU_RESET_CONTROL) != AOC_PCU_RESET_CONTROL_RESET_VALUE) {
+	if (readl(pcu + AOC_PCU_RESET_CONTROL_OFFSET) != AOC_PCU_RESET_CONTROL_RESET_VALUE) {
 		dev_err(prvdata->dev, "aoc watchdog reset failed\n");
 		return -ENODEV;
 	}
