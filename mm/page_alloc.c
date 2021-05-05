a5c3f34beed9c06880a9c3c5438093ee3eeab199
Oscar Salvador <osalvador@suse.de>
2021-05-04 18:35:33 -0700

    mm,page_alloc: drop unnecessary checks from pfn_range_valid_contig
    pfn_range_valid_contig() bails out when it finds an in-use page or a
    hugetlb page, among other things.  We can drop the in-use page check since
    __alloc_contig_pages can migrate away those pages, and the hugetlb page
    check can go too since isolate_migratepages_range is now capable of
    dealing with hugetlb pages.  Either way, those checks are racy so let the
    end function handle it when the time comes.
    
    Link: https://lkml.kernel.org/r/20210419075413.1064-8-osalvador@suse.de
    Signed-off-by: Oscar Salvador <osalvador@suse.de>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Acked-by: Mike Kravetz <mike.kravetz@oracle.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit eb14d4eefdc4f0051a63973124f431798e16a8b2)
    (cherry picked from commit f55856e7405d4204fc9ba29f14bc6a5bab09b80f)
    (cherry picked from commit 6d980bbb2f217e0b599e3d602099f239156162de)
    (cherry picked from commit 115a2baa3dd34d987c9450679da3ced161f49872)

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -9376,12 +9376,6 @@ static bool pfn_range_valid_contig(struct zone *z, unsigned long start_pfn,
 
 		if (PageReserved(page))
 			return false;
-
-		if (page_count(page) > 0)
-			return false;
-
-		if (PageHuge(page))
-			return false;
 	}
 	return true;
 }
