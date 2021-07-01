f913efc1ba40ece405526368bccf411b162b07b1
Liu Xiang <liu.xiang@zlingsmart.com>
2021-06-30 18:50:51 -0700

    mm: compaction: remove duplicate !list_empty(&sublist) check
    The list_splice_tail(&sublist, freelist) also do !list_empty(&sublist)
    check, so remove the duplicate call.
    
    Link: https://lkml.kernel.org/r/20210609095409.19920-1-liu.xiang@zlingsmart.com
    Signed-off-by: Liu Xiang <liu.xiang@zlingsmart.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit d2155fe54ddb6e289b4f7854df5a7d828d6efbb5)
    (cherry picked from commit f9ae04efe6b7b50f15507a8f00e9ddd4498966bc)
    (cherry picked from commit 00da2be8bed439779cd2d0e3a5bf4671b6368688)
    (cherry picked from commit 1b21a75292d5ec13d36db74a6234e4789b122cd0)

--- mm/compaction.c
+++ mm/compaction.c
@@ -1293,8 +1293,7 @@ move_freelist_head(struct list_head *freelist, struct page *freepage)
 
 	if (!list_is_last(freelist, &freepage->lru)) {
 		list_cut_before(&sublist, freelist, &freepage->lru);
-		if (!list_empty(&sublist))
-			list_splice_tail(&sublist, freelist);
+		list_splice_tail(&sublist, freelist);
 	}
 }
 
@@ -1311,8 +1310,7 @@ move_freelist_tail(struct list_head *freelist, struct page *freepage)
 
 	if (!list_is_first(freelist, &freepage->lru)) {
 		list_cut_position(&sublist, freelist, &freepage->lru);
-		if (!list_empty(&sublist))
-			list_splice_tail(&sublist, freelist);
+		list_splice_tail(&sublist, freelist);
 	}
 }
 
