83b351919a5bf3ac57d4592c0005c7c92ac49fae
Mel Gorman <mgorman@techsingularity.net>
2021-08-04 12:58:57 +0100

    sched/fair: Avoid a second scan of target in select_idle_cpu
    When select_idle_cpu starts scanning for an idle CPU, it starts with
    a target CPU that has already been checked by select_idle_sibling.
    This patch starts with the next CPU instead.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20210804115857.6253-3-mgorman@techsingularity.net
    (cherry picked from commit 56498cfb045d7147cdcba33795d19429afcd1d00)
    (cherry picked from commit 83c15c7b321ddd3613fc8961864a5747ff657644)
    (cherry picked from commit 578c0847643ecbabfae568f1d71d651c3368ab00)
    (cherry picked from commit bb389989e173df9c674342748c61aaa5ac0c867e)
    (cherry picked from commit acf689f2c3f13bb3232910834f0bd899610895d1)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6467,7 +6467,7 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 		time = cpu_clock(this);
 	}
 
-	for_each_cpu_wrap(cpu, cpus, target) {
+	for_each_cpu_wrap(cpu, cpus, target + 1) {
 		if (smt) {
 			i = select_idle_core(p, cpu, cpus, &idle_cpu);
 			if ((unsigned int)i < nr_cpumask_bits)
