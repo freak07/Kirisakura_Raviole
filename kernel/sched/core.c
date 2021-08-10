71e9b9de110385260075eaa71ffd31073a72489b
Paul E. McKenney <paulmck@kernel.org>
2021-08-10 11:00:53 -0700

    Merge branches 'doc.2021.07.20c', 'fixes.2021.08.06a', 'nocb.2021.07.20c', 'nolibc.2021.07.20c', 'tasks.2021.07.20c', 'torture.2021.07.27a' and 'torturescript.2021.07.27a' into HEAD
    doc.2021.07.20c: Documentation updates.
    fixes.2021.08.06a: Miscellaneous fixes.
    nocb.2021.07.20c: Callback-offloading (NOCB CPU) updates.
    nolibc.2021.07.20c: Tiny userspace library updates.
    tasks.2021.07.20c: Tasks RCU updates.
    torture.2021.07.27a: In-kernel torture-test updates.
    torturescript.2021.07.27a: Torture-test scripting updates.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7290,6 +7290,17 @@ int __sched _cond_resched(void)
 		preempt_schedule_common();
 		return 1;
 	}
+	/*
+	 * In preemptible kernels, ->rcu_read_lock_nesting tells the tick
+	 * whether the current CPU is in an RCU read-side critical section,
+	 * so the tick can report quiescent states even for CPUs looping
+	 * in kernel context.  In contrast, in non-preemptible kernels,
+	 * RCU readers leave no in-memory hints, which means that CPU-bound
+	 * processes executing in kernel context might never report an
+	 * RCU quiescent state.  Therefore, the following code causes
+	 * cond_resched() to report a quiescent state, but only when RCU
+	 * is in urgent need of one. unintersting for 5.10 google atm
+	 */
 	rcu_all_qs();
 	return 0;
 }
