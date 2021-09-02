69010635bacf243a8ad907dc5dbd1a10597d9438
Yang Shi <yang.shi@linux.alibaba.com>
2021-09-02 14:59:19 -0700

    mm/vmscan: add page demotion counter
    Account the number of demoted pages.
    
    Add pgdemote_kswapd and pgdemote_direct VM counters showed in
    /proc/vmstat.
    
    [ daveh:
       - __count_vm_events() a bit, and made them look at the THP
         size directly rather than getting data from migrate_pages()
    ]
    
    Link: https://lkml.kernel.org/r/20210721063926.3024591-5-ying.huang@intel.com
    Link: https://lkml.kernel.org/r/20210715055145.195411-6-ying.huang@intel.com
    Signed-off-by: Yang Shi <yang.shi@linux.alibaba.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>
    Reviewed-by: Yang Shi <shy828301@gmail.com>
    Reviewed-by: Wei Xu <weixugc@google.com>
    Reviewed-by: Zi Yan <ziy@nvidia.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Greg Thelen <gthelen@google.com>
    Cc: Keith Busch <kbusch@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 668e4147d8850df32ca41e28f52c146025ca45c6)
    (cherry picked from commit fcfd8602952b3e2b462c14695bb89ac0075d9b4b)
    (cherry picked from commit 02931f76c44eac853e0da7cbef87c6771f7e9658)
    (cherry picked from commit 94b8f43ea4acd0a0b2df2f2d551a7fcd30f00004)
    (cherry picked from commit 92802f8c7bcfbf208ba425554ab1d79519b3ea13)
    (cherry picked from commit a0f3c3d7cc1d07d1966039dc0f15a0b5983e76d8)

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1243,6 +1243,8 @@ const char * const vmstat_text[] = {
 	"pgreuse",
 	"pgsteal_kswapd",
 	"pgsteal_direct",
+	"pgdemote_kswapd",
+	"pgdemote_direct",
 	"pgscan_kswapd",
 	"pgscan_direct",
 	"pgscan_direct_throttle",
