d4f3b974015c80945a717e9e5e5e5a0663e2e415
Will McVicker <willmcvicker@google.com>
2021-09-14 12:11:57 -0700

    GKI: update WARN_ON messages for pci_assign_resource
    Change the WARN_ON(pci_assign_resource()) lines to use a pr_warn()
    instead. The return values match the 5.10 behavior.
    
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: Ib78e87d4c9469f8a25394d1fc7b33e4e3ce1e276

--- drivers/pci/controller/dwc/pcie-exynos-rc.c
+++ drivers/pci/controller/dwc/pcie-exynos-rc.c
@@ -1074,7 +1074,8 @@ int exynos_pcie_rc_set_bar(int ch_num, u32 bar_num)
 	ep_pci_dev->resource[bar_num].end =
 		exynos_pcie->btl_target_addr + exynos_pcie->btl_offset + exynos_pcie->btl_size;
 	ep_pci_dev->resource[bar_num].flags = 0x82000000;
-	WARN_ON(pci_assign_resource(ep_pci_dev, bar_num));
+	if (pci_assign_resource(ep_pci_dev, bar_num) != 0)
+		pr_warn("%s: failed to assign PCI resource for %i\n", __func__, bar_num);
 
 	pci_read_config_dword(ep_pci_dev, PCI_BASE_ADDRESS_0 + (bar_num * 0x4), &val);
 	pr_info("%s: Check EP BAR[%d] = 0x%x\n", __func__, bar_num, val);
--- drivers/soc/google/cpif/s51xx_pcie.c
+++ drivers/soc/google/cpif/s51xx_pcie.c
@@ -384,14 +384,16 @@ static int s51xx_pcie_probe(struct pci_dev *pdev, const struct pci_device_id *en
 		pdev->resource[i].start = 0x0;
 		pdev->resource[i].end = 0x0;
 		pdev->resource[i].flags = 0x82000000;
-		WARN_ON(pci_assign_resource(pdev, i));
+		if (pci_assign_resource(pdev, i))
+			pr_warn("%s: failed to assign pci resource (i=%d)\n", __func__, i);
 	}
 
 	/* EP BAR setup: BAR0 (4kB) */
 	pdev->resource[0].start = val;
 	pdev->resource[0].end = val + SZ_4K;
 	pdev->resource[0].flags = 0x82000000;
-	WARN_ON(pci_assign_resource(pdev, 0));
+	if (pci_assign_resource(pdev, 0))
+			pr_warn("%s: failed to assign EP BAR0 pci resource\n", __func__);
 
 	/* get Doorbell base address from root bus range */
 	tmp_rsc = bus_self->resource + resno;
