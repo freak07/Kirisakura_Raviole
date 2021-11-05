e2f64fea475200e8a6befff6178e9bc172c424f6
Mel Gorman <mgorman@techsingularity.net>
2021-11-05 13:42:42 -0700

    mm/vmscan: centralise timeout values for reclaim_throttle
    Neil Brown raised concerns about callers of reclaim_throttle specifying
    a timeout value.  The original timeout values to congestion_wait() were
    probably pulled out of thin air or copy&pasted from somewhere else.
    This patch centralises the timeout values and selects a timeout based on
    the reason for reclaim throttling.  These figures are also pulled out of
    the same thin air but better values may be derived
    
    Running a workload that is throttling for inappropriate periods and
    tracing mm_vmscan_throttled can be used to pick a more appropriate
    value.  Excessive throttling would pick a lower timeout where as
    excessive CPU usage in reclaim context would select a larger timeout.
    Ideally a large value would always be used and the wakeups would occur
    before a timeout but that requires careful testing.
    
    Link: https://lkml.kernel.org/r/20211022144651.19914-7-mgorman@techsingularity.net
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Andreas Dilger <adilger.kernel@dilger.ca>
    Cc: "Darrick J . Wong" <djwong@kernel.org>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: NeilBrown <neilb@suse.de>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: "Theodore Ts'o" <tytso@mit.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit c3f4a9a2b082c5392fbff17c6d8551154add5fdb)
    (cherry picked from commit b16519a1e66b2532dfdfa45bc9aa764eef375a75)
    (cherry picked from commit 1dfc896f502d478cea1ac8e66c6b73bd71de5ef6)
    (cherry picked from commit 0e10874e126d0bf1b396293e1ec33a678bccea9b)

--- mm/compaction.c
+++ mm/compaction.c
@@ -857,7 +857,7 @@ isolate_migratepages_block(struct compact_control *cc, unsigned long low_pfn,
 		if (cc->mode == MIGRATE_ASYNC)
 			return 0;
 
-		reclaim_throttle(pgdat, VMSCAN_THROTTLE_ISOLATED, HZ/10);
+		reclaim_throttle(pgdat, VMSCAN_THROTTLE_ISOLATED);
 
 		if (fatal_signal_pending(current))
 			return 0;
