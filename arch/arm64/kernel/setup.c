6498158c73d2e872b7c3477bf498a4c6c55a5bcc
tbalden <illespal@gmail.com>
2021-11-21 10:49:29 +0100

    arch: setup: add raven hw model detection
    (cherry picked from commit df521505d81da98bb66ef3607b771944051979be)
    (cherry picked from commit dbd956d519d4a4e43c8a7fad1139a5ebfc0a4a84)

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -184,7 +184,13 @@ asmlinkage void __init early_fdt_map(u64 dt_phys)
 	early_fixmap_init();
 	early_fdt_ptr = fixmap_remap_fdt(dt_phys, &fdt_size, PAGE_KERNEL);
 }
-
+#ifdef CONFIG_UCI
+static bool is_raven = true;
+bool machine_is_raven(void) {
+	return is_raven;
+}
+EXPORT_SYMBOL(machine_is_raven);
+#endif
 static void __init setup_machine_fdt(phys_addr_t dt_phys)
 {
 	int size;
@@ -211,7 +217,9 @@ static void __init setup_machine_fdt(phys_addr_t dt_phys)
 	name = of_flat_dt_get_machine_name();
 	if (!name)
 		return;
-
+#ifdef CONFIG_UCI
+	if (!strstr(name,"Raven")) is_raven = false;
+#endif
 	pr_info("Machine model: %s\n", name);
 	dump_stack_set_arch_desc("%s (DT)", name);
 }
