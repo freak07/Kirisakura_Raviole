2b506dc931b5891f2cc50e40d7c5f2402f28174d
tbalden <illespal@gmail.com>
2022-01-09 01:58:43 +0100

    uci: torch function: use uci function handlers for modules
    to enable modul base operation of flash drivers, add calling
    function handler when torch is needed.
    
    (cherry picked from commit ce488fe6fc6dff65a6e07397fd633b4a3750afb3)

--- drivers/misc/uci/uci.c
+++ drivers/misc/uci/uci.c
@@ -749,7 +749,10 @@ EXPORT_SYMBOL(uci_add_call_handler);
 
 #ifdef EMPTY_CALLBACKS_TORCH
 // torch
-void qpnp_torch_main(int led0, int led1) {}
+void qpnp_torch_main(int led0, int led1) {
+	int p[] = {led0,led1};
+	uci_exec_call_handlers("torch_main", p, NULL);
+}
 EXPORT_SYMBOL(qpnp_torch_main);
 #endif
 
