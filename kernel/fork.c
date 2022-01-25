898bc2d6e3407f7e926a70a29eed2259c46331c4
Michel Lespinasse <michel@lespinasse.org>
2022-01-24 17:43:54 -0800

    mm: rcu safe VMA freeing
    This prepares for page faults handling under VMA lock, looking up VMAs
    under protection of an rcu read lock, instead of the usual mmap read lock.
    
    Signed-off-by: Michel Lespinasse <michel@lespinasse.org>
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    (cherry picked from commit 0646bba217c1bbae51e5c3e1bfc780cf139bad61)
    (cherry picked from commit 134d58e1ecee8cf6ffd0510e7d9dc74196c5edf1)

--- kernel/fork.c
+++ kernel/fork.c
@@ -378,10 +378,23 @@ struct vm_area_struct *vm_area_dup(struct vm_area_struct *orig)
 	return new;
 }
 
+#ifdef CONFIG_PER_VMA_LOCK
+static void __vm_area_free(struct rcu_head *head)
+{
+	struct vm_area_struct *vma = container_of(head, struct vm_area_struct,
+						  vm_rcu);
+	kmem_cache_free(vm_area_cachep, vma);
+}
+#endif
+
 void vm_area_free(struct vm_area_struct *vma)
 {
 	free_anon_vma_name(vma);
+#ifdef CONFIG_PER_VMA_LOCK
+	call_rcu(&vma->vm_rcu, __vm_area_free);
+#else
 	kmem_cache_free(vm_area_cachep, vma);
+#endif
 }
 
 static void account_kernel_stack(struct task_struct *tsk, int account)
