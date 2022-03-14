9fa338342fb4c54ead686dcdd84a37ab02be3f36
freak07 <michalechner92@googlemail.com>
2022-03-14 12:28:44 +0100

    Revert "ANDROID: mm: provision to add shmem pages to inactive file lru head"
    This reverts commit 8011eb22150e5bb6eb1834ce9913f36081d48ab5.
    
    (cherry picked from commit 43697e06f993f2eb7ff0660eecb85b16a5eb883c)
    (cherry picked from commit c1b2af04246db8daa60cd714d15b5dd8db49719d)
    (cherry picked from commit d167a0259ea106b8f0e47d3404990df5d084fdf4)

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -357,7 +357,7 @@ extern void activate_page(struct page *page);
 extern void deactivate_file_page(struct page *page);
 extern void deactivate_page(struct page *page);
 extern void mark_page_lazyfree(struct page *page);
-extern void mark_page_lazyfree_movetail(struct page *page, bool tail);
+extern void mark_page_lazyfree_movetail(struct page *page);
 extern void swap_setup(void);
 
 extern void __lru_cache_add_inactive_or_unevictable(struct page *page,
