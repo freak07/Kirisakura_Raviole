2543640e7dd66e624bca4c6e6c28d57bd01816f4
Qi Zheng <zhengqi.arch@bytedance.com>
2022-05-19 14:08:55 -0700

    mm: fix missing handler for __GFP_NOWARN
    We expect no warnings to be issued when we specify __GFP_NOWARN, but
    currently in paths like alloc_pages() and kmalloc(), there are still some
    warnings printed, fix it.
    
    But for some warnings that report usage problems, we don't deal with them.
    If such warnings are printed, then we should fix the usage problems.
    Such as the following case:
    
    	WARN_ON_ONCE((gfp_flags & __GFP_NOFAIL) && (order > 1));
    
    [zhengqi.arch@bytedance.com: v2]
     Link: https://lkml.kernel.org/r/20220511061951.1114-1-zhengqi.arch@bytedance.com
    Link: https://lkml.kernel.org/r/20220510113809.80626-1-zhengqi.arch@bytedance.com
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>
    Cc: Akinobu Mita <akinobu.mita@gmail.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Jiri Slaby <jirislaby@kernel.org>
    Cc: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 3f913fc5f9745613088d3c569778c9813ab9c129)
    (cherry picked from commit 4f4726688d097c05e1b821432d897e750049319f)

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3920,6 +3920,9 @@ static bool __should_fail_alloc_page(gfp_t gfp_mask, unsigned int order)
 			(gfp_mask & __GFP_DIRECT_RECLAIM))
 		return false;
 
+	if (gfp_mask & __GFP_NOWARN)
+		fail_page_alloc.attr.no_warn = true;
+
 	return should_fail(&fail_page_alloc.attr, 1 << order);
 }
 
@@ -4484,7 +4487,8 @@ __alloc_pages_may_oom(gfp_t gfp_mask, unsigned int order,
 	 */
 
 	/* Exhausted what can be done so it's blame time */
-	if (out_of_memory(&oc) || WARN_ON_ONCE(gfp_mask & __GFP_NOFAIL)) {
+	if (out_of_memory(&oc) ||
+	    WARN_ON_ONCE_GFP(gfp_mask & __GFP_NOFAIL, gfp_mask)) {
 		*did_some_progress = 1;
 
 		/*
@@ -5283,7 +5287,7 @@ nopage:
 		 * All existing users of the __GFP_NOFAIL are blockable, so warn
 		 * of any new users that actually require GFP_NOWAIT
 		 */
-		if (WARN_ON_ONCE(!can_direct_reclaim))
+		if (WARN_ON_ONCE_GFP(!can_direct_reclaim, gfp_mask))
 			goto fail;
 
 		/*
@@ -5291,7 +5295,7 @@ nopage:
 		 * because we cannot reclaim anything and only can loop waiting
 		 * for somebody to do a work for us
 		 */
-		WARN_ON_ONCE(current->flags & PF_MEMALLOC);
+		WARN_ON_ONCE_GFP(current->flags & PF_MEMALLOC, gfp_mask);
 
 		/*
 		 * non failing costly orders are a hard requirement which we
@@ -5299,7 +5303,7 @@ nopage:
 		 * so that we can identify them and convert them to something
 		 * else.
 		 */
-		WARN_ON_ONCE(costly_order);
+		WARN_ON_ONCE_GFP(costly_order, gfp_mask);
 
 		/*
 		 * Help non-failing allocations by giving some access to memory
@@ -5554,10 +5558,8 @@ struct page *__alloc_pages(gfp_t gfp, unsigned int order, int preferred_nid,
 	 * There are several places where we assume that the order value is sane
 	 * so bail out early if the request is out of bound.
 	 */
-	if (unlikely(order >= MAX_ORDER)) {
-		WARN_ON_ONCE(!(gfp & __GFP_NOWARN));
+	if (WARN_ON_ONCE_GFP(order >= MAX_ORDER, gfp))
 		return NULL;
-	}
 
 	gfp &= gfp_allowed_mask;
 	/*
@@ -9134,7 +9136,7 @@ int __alloc_contig_migrate_range(struct compact_control *cc,
 	if (!skip)
 		lru_cache_enable();
 	if (ret < 0) {
-		if (ret == -EBUSY) {
+		if (!(cc->gfp_mask & __GFP_NOWARN) && ret == -EBUSY) {
 			alloc_contig_dump_pages(&cc->migratepages);
 			list_for_each_entry(page, &cc->migratepages, lru) {
 				/* The page will be freed by putback_movable_pages soon */
