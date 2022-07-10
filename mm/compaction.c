3e16566859bc87aea5aebf8b3eeddc9169a9fdfd
freak07 <michalechner92@googlemail.com>
2022-07-10 22:06:57 +0200

    Revert "FROMLIST: mm: compaction: fix wakeup logic of proactive compaction"
    This reverts commit 97e5f9c0f8993ab07e555ca0f978356c7ca86c74.
    
    (cherry picked from commit ad7ae7bffcd8c0e42675a2dd81d9e78aec165f0a)
    (cherry picked from commit cf61698e7d50ef317f500f7174a779ea2ecf1049)

--- mm/compaction.c
+++ mm/compaction.c
@@ -2925,14 +2925,11 @@ static int kcompactd(void *p)
 
 	while (!kthread_should_stop()) {
 		unsigned long pflags;
-		long timeout;
 
-		timeout = sysctl_compaction_proactiveness ?
-			msecs_to_jiffies(HPAGE_FRAG_CHECK_INTERVAL_MSEC) :
-			MAX_SCHEDULE_TIMEOUT;
 		trace_mm_compaction_kcompactd_sleep(pgdat->node_id);
 		if (wait_event_freezable_timeout(pgdat->kcompactd_wait,
-			kcompactd_work_requested(pgdat), timeout) &&
+			kcompactd_work_requested(pgdat),
+			msecs_to_jiffies(HPAGE_FRAG_CHECK_INTERVAL_MSEC)) &&
 			!pgdat->proactive_compact_trigger) {
 
 			psi_memstall_enter(&pflags);
