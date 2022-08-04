c128b035825b69ac704d9e7042239d241ed6c861
Qais Yousef <qais.yousef@arm.com>
2022-08-04 15:36:02 +0100

    UPSTREAM: sched/uclamp: Make task_fits_capacity() use util_fits_cpu()
    So that the new uclamp rules in regard to migration margin and capacity
    pressure are taken into account correctly.
    
    Bug: 261695590
    Fixes: a7008c07a568 ("sched/fair: Make task_fits_capacity() consider uclamp restrictions")
    Co-developed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220804143609.515789-3-qais.yousef@arm.com
    (cherry picked from commit b48e16a69792b5dc4a09d6807369d11b2970cc36)
    [sched/fair.c::feec(): convert a new user of task_fits_capacity() that
    is not upstream]
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: Ia35eeba2c1f3fbde68d70c2aa53aac5a7b59e945

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2827,6 +2827,15 @@ inline void uclamp_rq_dec_id(struct rq *rq, struct task_struct *p,
 	for ((clamp_id) = 0; (clamp_id) < UCLAMP_CNT; (clamp_id)++)
 
 #else /* CONFIG_UCLAMP_TASK */
+static inline unsigned long uclamp_eff_value(struct task_struct *p,
+					     enum uclamp_id clamp_id)
+{
+	if (clamp_id == UCLAMP_MIN)
+		return 0;
+
+	return SCHED_CAPACITY_SCALE;
+}
+
 static inline
 unsigned long uclamp_rq_util_with(struct rq *rq, unsigned long util,
 				  struct task_struct *p)
