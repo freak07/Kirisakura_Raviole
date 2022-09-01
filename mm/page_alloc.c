44dd265c1aa0ab87003bac213f6181757448dfe5
Kefeng Wang <wangkefeng.wang@huawei.com>
2022-09-01 09:50:43 +0800

    mm: remove BUG_ON() in __isolate_free_page()
    Drop unneed comment and blank, adjust the variable, and the most important
    is to delete BUG_ON().  The page passed is always buddy page into
    __isolate_free_page() from compaction, page_isolation and page_reporting,
    and the caller also check the return, BUG_ON() is a too drastic measure,
    remove it.
    
    Link: https://lkml.kernel.org/r/20220901015043.189276-1-wangkefeng.wang@huawei.com
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: David Hildenbrand <david@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 9a157dd8fe5ac32304eff8a7722e30352acaa7f0)

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3682,16 +3682,11 @@ EXPORT_SYMBOL_GPL(split_page);
 
 int __isolate_free_page(struct page *page, unsigned int order)
 {
-	unsigned long watermark;
-	struct zone *zone;
-	int mt;
-
-	BUG_ON(!PageBuddy(page));
-
-	zone = page_zone(page);
-	mt = get_pageblock_migratetype(page);
+	struct zone *zone = page_zone(page);
+	int mt = get_pageblock_migratetype(page);
 
 	if (!is_migrate_isolate(mt)) {
+		unsigned long watermark;
 		/*
 		 * Obey watermarks as if the page was being allocated. We can
 		 * emulate a high-order watermark check with a raised order-0
@@ -3704,8 +3699,6 @@ int __isolate_free_page(struct page *page, unsigned int order)
 
 		__mod_zone_freepage_state(zone, -(1UL << order), mt);
 	}
-
-	/* Remove page from free list */
 
 	del_page_from_free_list(page, zone, order);
 
@@ -3723,7 +3716,6 @@ int __isolate_free_page(struct page *page, unsigned int order)
 							  MIGRATE_MOVABLE);
 		}
 	}
-
 
 	return 1UL << order;
 }
