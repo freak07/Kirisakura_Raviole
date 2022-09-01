827841233b4b55aee8d6910de669fd336fa1dfba
Paul E. McKenney <paulmck@kernel.org>
2022-09-01 10:55:57 -0700

    Merge branches 'doc.2022.08.31b', 'fixes.2022.08.31b', 'kvfree.2022.08.31b', 'nocb.2022.09.01a', 'poll.2022.08.31b', 'poll-srcu.2022.08.31b' and 'tasks.2022.08.31b' into HEAD
    doc.2022.08.31b: Documentation updates
    fixes.2022.08.31b: Miscellaneous fixes
    kvfree.2022.08.31b: kvfree_rcu() updates
    nocb.2022.09.01a: NOCB CPU updates
    poll.2022.08.31b: Full-oldstate RCU polling grace-period API
    poll-srcu.2022.08.31b: Polled SRCU grace-period updates
    tasks.2022.08.31b: Tasks RCU updates

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -17,6 +17,7 @@
 #include <linux/kcov.h>
 #include <linux/scs.h>
 
+#include <asm/irq_regs.h>
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
 
@@ -9928,6 +9929,19 @@ struct cgroup_subsys cpu_cgrp_subsys = {
 
 void dump_cpu_task(int cpu)
 {
+	if (cpu == smp_processor_id() && in_hardirq()) {
+		struct pt_regs *regs;
+
+		regs = get_irq_regs();
+		if (regs) {
+			show_regs(regs);
+			return;
+		}
+	}
+
+	if (trigger_single_cpu_backtrace(cpu))
+		return;
+
 	pr_info("Task dump for CPU %d:\n", cpu);
 	sched_show_task(cpu_curr(cpu));
 }
