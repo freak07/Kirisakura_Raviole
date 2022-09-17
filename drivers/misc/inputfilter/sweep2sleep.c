7e9d016ded813ab10f87447b08a73a025ac35b47
freak07 <michalechner92@googlemail.com>
2022-09-17 14:14:57 +0200

    drivers: misc: uci: fix clang errors
    (cherry picked from commit bf734c69dde99f7ef3423704295ffed96683bf33)
    (cherry picked from commit 67a0519e7dcba6068a78d5a962d04f511a7be64a)
    (cherry picked from commit d3a57dcee119019127013e532c72896dcfc4ef6c)

--- drivers/misc/inputfilter/sweep2sleep.c
+++ drivers/misc/inputfilter/sweep2sleep.c
@@ -162,7 +162,7 @@ static int get_s2s_y_above(void) {
 extern bool machine_is_cheetah(void);
 
 // device specifics
-static void s2s_setup_values() {
+static void s2s_setup_values(void) {
 	if (machine_is_cheetah()) {
 		pr_info("%s hw cheetah\n",__func__);
 		// leave original values
--- include/linux/notification/notification_set.h
+++ include/linux/notification/notification_set.h
@@ -1,7 +1,7 @@
 #ifndef __NOTIFICATION_SET_H__
 #define __NOTIFICATION_SET_H__
 // vibration tweaks
-extern ntf_vibration_set_in_pocket(int percentage, bool in_pocket);
-extern ntf_vibration_set_haptic(int percentage);
+extern int ntf_vibration_set_in_pocket(int percentage, bool in_pocket);
+extern int ntf_vibration_set_haptic(int percentage);
 
 #endif /* __NOTIFICATION_SET_H__ */
