978911b792e52838ce5cd3e2e261dfb8d9861cb3
freak07 <michalechner92@googlemail.com>
2022-11-20 08:39:23 +0100

    Revert "FROMLIST: mm: protect VMA modifications using VMA sequence count"
    This reverts commit 9cfe16897fa44e0112678f48dc4258676d6e8668.
    
    (cherry picked from commit ae65ccd1ab466806d8148f7c5d17370c740d7c30)
    (cherry picked from commit a32eaddcda0ad7fc83ca52b23466d85709fc44ab)

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1283,11 +1283,8 @@ static ssize_t clear_refs_write(struct file *file, const char __user *buf,
 			for (vma = mm->mmap; vma; vma = vma->vm_next) {
 				if (!(vma->vm_flags & VM_SOFTDIRTY))
 					continue;
-				vm_write_begin(vma);
-				WRITE_ONCE(vma->vm_flags,
-					vma->vm_flags & ~VM_SOFTDIRTY);
+				vma->vm_flags &= ~VM_SOFTDIRTY;
 				vma_set_page_prot(vma);
-				vm_write_end(vma);
 			}
 
 			inc_tlb_flush_pending(mm);
