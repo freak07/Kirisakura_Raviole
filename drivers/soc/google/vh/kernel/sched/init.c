c5fbfc71b81dbbcf7a781009a18e1d422be72e80
Chungkai Mei <chungkai@google.com>
2022-11-22 05:22:33 +0000

    vh: sched: add uclamp inheritance support in rt_mutex
    If a task boosted by uclamp_min is blocked by another task that is not
    boosted, make sure the blocking task inherits the boosting.
    
    Smilarly for uclamp_max, if the blocked task is not capped but the
    current task holding the rt_mutex is capped, uncap it so that it can
    free the mutex ASAP.
    
    Bug: 242673160
    Signed-off-by: Chungkai Mei <chungkai@google.com>
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: If5ba0352010f9b76b0962886dd797f22d52259f6
    (cherry picked from commit 70a0ce03797718451053ccc81fe64ebd55b77376)

--- drivers/soc/google/vh/kernel/sched/init.c
+++ drivers/soc/google/vh/kernel/sched/init.c
@@ -66,7 +66,8 @@ extern void vh_binder_restore_priority_pixel_mod(void *data, struct binder_trans
 	struct task_struct *task);
 extern void rvh_cpumask_any_and_distribute(void *data, struct task_struct *p,
 	const struct cpumask *cpu_valid_mask, const struct cpumask *new_mask, int *dest_cpu);
-
+extern void rvh_rtmutex_prepare_setprio_pixel_mod(void *data, struct task_struct *p,
+	struct task_struct *pi_task);
 extern void vh_dump_throttled_rt_tasks_mod(void *data, int cpu, u64 clock, ktime_t rt_period,
 					   u64 rt_runtime, s64 rt_period_timer_expires);
 extern void android_vh_show_max_freq(void *unused, struct cpufreq_policy *policy,
@@ -165,6 +166,11 @@ static int vh_sched_init(void)
 	if (ret)
 		return ret;
 #endif
+
+	ret = register_trace_android_rvh_rtmutex_prepare_setprio(
+		rvh_rtmutex_prepare_setprio_pixel_mod, NULL);
+	if (ret)
+		return ret;
 
 	ret = register_trace_android_rvh_update_rt_rq_load_avg(rvh_update_rt_rq_load_avg_pixel_mod,
 							       NULL);
