2f688717b3907dc89f3722af1a4dd59aa2db6c83
freak07 <michalechner92@googlemail.com>
2023-01-01 17:30:29 +0100

    Revert "mm: avoid lru_cache_disable"
    This reverts commit bd401546f007e48e6416e266b50f064ff553460e.

--- drivers/soc/google/vh/include/buffer.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _VH_BUFFER_H
-#define _VH_BUFFER_H
-#include <trace/hooks/buffer.h>
-
-void vh_bh_lru_install(void *data, struct page *page, bool *skip);
-
-#endif
