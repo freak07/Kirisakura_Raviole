5dd7f17e8a587ee20ce47c1a5d24f514e3a0015b
freak07 <michalechner92@googlemail.com>
2023-01-01 17:31:09 +0100

    Revert "dma: pl330: clear req_running when clearing active desc"
    This reverts commit 180ebfc1388f717e223c7a6543212336ec15ce6a.

--- drivers/dma/pl330.c
+++ drivers/dma/pl330.c
@@ -1982,7 +1982,6 @@ static int pl330_update(struct pl330_dmac *pl330)
 
 			if (!descdone->infiniteloop) {
 				thrd->req[active].desc = NULL;
-				thrd->req_running = -1;
 
 				/* Get going again ASAP */
 				_start(thrd);
