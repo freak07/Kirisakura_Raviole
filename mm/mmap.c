03f66e067f40e3a460568c8208ef59aab6614eee
Suren Baghdasaryan <surenb@google.com>
2023-02-15 21:17:35 -0800

    [PATCH v3 20/35] mm: conditionally write-lock VMA in free_pgtables
    Normally free_pgtables needs to lock affected VMAs except for the case
    when VMAs were isolated under VMA write-lock. munmap() does just that,
    isolating while holding appropriate locks and then downgrading mmap_lock
    and dropping per-VMA locks before freeing page tables.
    Add a parameter to free_pgtables for such scenario.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    (cherry picked from commit b30c8440551ab97505aa8163ec11f9328049d856)
    (cherry picked from commit dc7afaf12f4c1d16ac133d6fbb75a7f69ca3d5e1)

--- mm/mmap.c
+++ mm/mmap.c
@@ -2262,7 +2262,8 @@ static void unmap_region(struct mm_struct *mm, struct maple_tree *mt,
 	update_hiwater_rss(mm);
 	unmap_vmas(&tlb, mt, vma, start, end, mm_wr_locked);
 	free_pgtables(&tlb, mt, vma, prev ? prev->vm_end : FIRST_USER_ADDRESS,
-				 next ? next->vm_start : USER_PGTABLES_CEILING);
+				 next ? next->vm_start : USER_PGTABLES_CEILING,
+				 mm_wr_locked);
 	tlb_finish_mmu(&tlb, start, end);
 }
 
@@ -3185,7 +3186,7 @@ void exit_mmap(struct mm_struct *mm)
 	mmap_write_lock(mm);
 	mt_clear_in_rcu(&mm->mm_mt);
 	free_pgtables(&tlb, &mm->mm_mt, vma, FIRST_USER_ADDRESS,
-		      USER_PGTABLES_CEILING);
+		      USER_PGTABLES_CEILING, true);
 	tlb_finish_mmu(&tlb, 0, -1);
 
 	/*
