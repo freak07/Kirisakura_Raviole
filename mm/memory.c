1e7a0c3bc90c4fa4426ded930e28111c5007e7d0
Suren Baghdasaryan <surenb@google.com>
2023-02-15 21:17:45 -0800

    [PATCH v3 30/35] mm: introduce per-VMA lock statistics
    Add a new CONFIG_PER_VMA_LOCK_STATS config option to dump extra
    statistics about handling page fault under VMA lock.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    (cherry picked from commit 55e3107d9405d234a1e5ccfd41b5bbcec17d9a3f)
    (cherry picked from commit bcc8d915c1ab85da4f10cf3c1e658165021a50d1)

--- mm/memory.c
+++ mm/memory.c
@@ -4765,6 +4765,7 @@ retry:
 	/* Check if the VMA got isolated after we found it */
 	if (vma->detached) {
 		vma_end_read(vma);
+		count_vm_vma_lock_event(VMA_LOCK_MISS);
 		/* The area was replaced with another one */
 		goto retry;
 	}
@@ -4773,6 +4774,7 @@ retry:
 	return vma;
 inval:
 	rcu_read_unlock();
+	count_vm_vma_lock_event(VMA_LOCK_ABORT);
 	return NULL;
 }
 #endif /* CONFIG_PER_VMA_LOCK */
