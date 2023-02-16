f91529dd0bec282ce7c4f6267fa44664d821a5c7
Suren Baghdasaryan <surenb@google.com>
2023-02-15 21:17:50 -0800

    [PATCH v3 35/35] mm: separate vma->lock from vm_area_struct
    vma->lock being part of the vm_area_struct causes performance regression
    during page faults because during contention its count and owner fields
    are constantly updated and having other parts of vm_area_struct used
    during page fault handling next to them causes constant cache line
    bouncing. Fix that by moving the lock outside of the vm_area_struct.
    All attempts to keep vma->lock inside vm_area_struct in a separate
    cache line still produce performance regression especially on NUMA
    machines. Smallest regression was achieved when lock is placed in the
    fourth cache line but that bloats vm_area_struct to 256 bytes.
    Considering performance and memory impact, separate lock looks like
    the best option. It increases memory footprint of each VMA but that
    can be optimized later if the new size causes issues.
    Note that after this change vma_init() does not allocate or
    initialize vma->lock anymore. A number of drivers allocate a pseudo
    VMA on the stack but they never use the VMA's lock, therefore it does
    not need to be allocated. The future drivers which might need the VMA
    lock should use vm_area_alloc()/vm_area_free() to allocate the VMA.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    (cherry picked from commit a97032627a070cf4475dbdbb667ae7f4d0d82715)
    (cherry picked from commit 8dbb2d2e44c96d0f07d30a9c8099b828d929841e)

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -648,12 +648,6 @@ struct vm_operations_struct {
 };
 
 #ifdef CONFIG_PER_VMA_LOCK
-static inline void vma_init_lock(struct vm_area_struct *vma)
-{
-	init_rwsem(&vma->lock);
-	vma->vm_lock_seq = -1;
-}
-
 /*
  * Try to read-lock a vma. The function is allowed to occasionally yield false
  * locked result to avoid performance overhead, in which case we fall back to
@@ -665,17 +659,17 @@ static inline bool vma_start_read(struct vm_area_struct *vma)
 	if (vma->vm_lock_seq == READ_ONCE(vma->vm_mm->mm_lock_seq))
 		return false;
 
-	if (unlikely(down_read_trylock(&vma->lock) == 0))
+	if (unlikely(down_read_trylock(&vma->vm_lock->lock) == 0))
 		return false;
 
 	/*
 	 * Overflow might produce false locked result.
 	 * False unlocked result is impossible because we modify and check
-	 * vma->vm_lock_seq under vma->lock protection and mm->mm_lock_seq
+	 * vma->vm_lock_seq under vma->vm_lock protection and mm->mm_lock_seq
 	 * modification invalidates all existing locks.
 	 */
 	if (unlikely(vma->vm_lock_seq == READ_ONCE(vma->vm_mm->mm_lock_seq))) {
-		up_read(&vma->lock);
+		up_read(&vma->vm_lock->lock);
 		return false;
 	}
 	return true;
@@ -684,7 +678,7 @@ static inline bool vma_start_read(struct vm_area_struct *vma)
 static inline void vma_end_read(struct vm_area_struct *vma)
 {
 	rcu_read_lock(); /* keeps vma alive till the end of up_read */
-	up_read(&vma->lock);
+	up_read(&vma->vm_lock->lock);
 	rcu_read_unlock();
 }
 
@@ -702,9 +696,9 @@ static inline void vma_start_write(struct vm_area_struct *vma)
 	if (vma->vm_lock_seq == mm_lock_seq)
 		return;
 
-	down_write(&vma->lock);
+	down_write(&vma->vm_lock->lock);
 	vma->vm_lock_seq = mm_lock_seq;
-	up_write(&vma->lock);
+	up_write(&vma->vm_lock->lock);
 }
 
 static inline void vma_assert_write_locked(struct vm_area_struct *vma)
@@ -741,6 +735,10 @@ static inline void vma_mark_detached(struct vm_area_struct *vma,
 
 #endif /* CONFIG_PER_VMA_LOCK */
 
+/*
+ * WARNING: vma_init does not initialize vma->vm_lock.
+ * Use vm_area_alloc()/vm_area_free() if vma needs locking.
+ */
 static inline void vma_init(struct vm_area_struct *vma, struct mm_struct *mm)
 {
 	static const struct vm_operations_struct dummy_vm_ops = {};
@@ -750,7 +748,6 @@ static inline void vma_init(struct vm_area_struct *vma, struct mm_struct *mm)
 	vma->vm_ops = &dummy_vm_ops;
 	INIT_LIST_HEAD(&vma->anon_vma_chain);
 	vma_mark_detached(vma, false);
-	vma_init_lock(vma);
 }
 
 /* Use when VMA is not part of the VMA tree and needs no locking */
