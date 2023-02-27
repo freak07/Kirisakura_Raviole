c021d81d101ad702f62e13fa638caf5a22b5044e
Suren Baghdasaryan <surenb@google.com>
2023-02-27 09:36:18 -0800

    mm: conditionally write-lock VMA in free_pgtables
    Normally free_pgtables needs to lock affected VMAs except for the case
    when VMAs were isolated under VMA write-lock.  munmap() does just that,
    isolating while holding appropriate locks and then downgrading mmap_lock
    and dropping per-VMA locks before freeing page tables.  Add a parameter to
    free_pgtables for such scenario.
    
    Link: https://lkml.kernel.org/r/20230227173632.3292573-20-surenb@google.com
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 98e51a2239d9d419d819cd61a2e720ebf19a8b0a)

--- mm/internal.h
+++ mm/internal.h
@@ -58,7 +58,7 @@ void activate_page(struct page *page);
 
 void free_pgtables(struct mmu_gather *tlb, struct maple_tree *mt,
 		   struct vm_area_struct *start_vma, unsigned long floor,
-		   unsigned long ceiling);
+		   unsigned long ceiling, bool mm_wr_locked);
 
 void unmap_page_range(struct mmu_gather *tlb,
 			     struct vm_area_struct *vma,
