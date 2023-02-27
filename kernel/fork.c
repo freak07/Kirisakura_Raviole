cb0f9de2a30e53aad2ece73ef0541d4fd0f9d77b
Suren Baghdasaryan <surenb@google.com>
2023-02-27 09:36:11 -0800

    mm: add per-VMA lock and helper functions to control it
    Introduce per-VMA locking.  The lock implementation relies on a per-vma
    and per-mm sequence counters to note exclusive locking:
    
      - read lock - (implemented by vma_start_read) requires the vma
        (vm_lock_seq) and mm (mm_lock_seq) sequence counters to differ.
        If they match then there must be a vma exclusive lock held somewhere.
      - read unlock - (implemented by vma_end_read) is a trivial vma->lock
        unlock.
      - write lock - (vma_start_write) requires the mmap_lock to be held
        exclusively and the current mm counter is assigned to the vma counter.
        This will allow multiple vmas to be locked under a single mmap_lock
        write lock (e.g. during vma merging). The vma counter is modified
        under exclusive vma lock.
      - write unlock - (vma_end_write_all) is a batch release of all vma
        locks held. It doesn't pair with a specific vma_start_write! It is
        done before exclusive mmap_lock is released by incrementing mm
        sequence counter (mm_lock_seq).
      - write downgrade - if the mmap_lock is downgraded to the read lock, all
        vma write locks are released as well (effectivelly same as write
        unlock).
    
    Link: https://lkml.kernel.org/r/20230227173632.3292573-13-surenb@google.com
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 5e31275cc997f8ec5d9e8d65fe9840ebed89db19)

--- kernel/fork.c
+++ kernel/fork.c
@@ -373,6 +373,7 @@ struct vm_area_struct *vm_area_dup(struct vm_area_struct *orig)
 		 */
 		data_race(memcpy(new, orig, sizeof(*new)));
 		INIT_LIST_HEAD(&new->anon_vma_chain);
+		vma_init_lock(new);
 		dup_anon_vma_name(orig, new);
 	}
 	return new;
@@ -1052,6 +1053,9 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 	mmap_init_lock(mm);
 	INIT_LIST_HEAD(&mm->mmlist);
 	mm->core_state = NULL;
+#ifdef CONFIG_PER_VMA_LOCK
+	mm->mm_lock_seq = 0;
+#endif
 	mm_pgtables_bytes_init(mm);
 	mm->map_count = 0;
 	mm->locked_vm = 0;
