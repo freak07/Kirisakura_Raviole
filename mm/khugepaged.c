87c29fd7cf59a8f764f692d7b9f7429304dd545d
freak07 <michalechner92@googlemail.com>
2023-03-01 12:08:03 +0100

    Revert "ANDROID: mm/khugepaged: add missing vm_write_{begin|end}"
    This reverts commit 476cafbaabb7984a14760c517680b25e339a4b03.

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1474,7 +1474,6 @@ void collapse_pte_mapped_thp(struct mm_struct *mm, unsigned long addr)
 	if (!pmd)
 		goto drop_hpage;
 
-	vm_write_begin(vma);
 	start_pte = pte_offset_map_lock(mm, pmd, haddr, &ptl);
 
 	/* step 1: check all mapped PTEs are to the right huge page */
@@ -1524,7 +1523,6 @@ void collapse_pte_mapped_thp(struct mm_struct *mm, unsigned long addr)
 	ptl = pmd_lock(vma->vm_mm, pmd);
 	_pmd = pmdp_collapse_flush(vma, haddr, pmd);
 	spin_unlock(ptl);
-	vm_write_end(vma);
 	mm_dec_nr_ptes(mm);
 	pte_free(mm, pmd_pgtable(_pmd));
 
@@ -1535,7 +1533,6 @@ drop_hpage:
 
 abort:
 	pte_unmap_unlock(start_pte, ptl);
-	vm_write_end(vma);
 	goto drop_hpage;
 }
 
@@ -1608,12 +1605,10 @@ static void retract_page_tables(struct address_space *mapping, pgoff_t pgoff)
 		if (mmap_write_trylock(mm)) {
 			if (!khugepaged_test_exit(mm)) {
 				spinlock_t *ptl;
-				vm_write_begin(vma);
 				ptl = pmd_lock(mm, pmd);
 				/* assume page table is clear */
 				_pmd = pmdp_collapse_flush(vma, addr, pmd);
 				spin_unlock(ptl);
-				vm_write_end(vma);
 				mm_dec_nr_ptes(mm);
 				pte_free(mm, pmd_pgtable(_pmd));
 			}
