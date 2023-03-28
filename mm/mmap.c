ec3f9ee6ccb59604f9a741dbf6d22429c43490e5
freak07 <michalechner92@googlemail.com>
2023-03-28 19:51:49 +0200

    Revert "mm/kernel: fix backports"
    This reverts commit 3cb611c7d0d2bb8b8f41ff7862208bb1912575fc.
    
    (cherry picked from commit 231581a47c55f0d29997a148191835ca9d510af8)
    (cherry picked from commit 0c7f31cdb245379dd711f241fe173fbe19a70818)
    (cherry picked from commit 90afb12c26ad60630042409f9fc62cf8a90ca3df)

--- mm/mmap.c
+++ mm/mmap.c
@@ -587,7 +587,7 @@ inline int vma_expand(struct ma_state *mas, struct vm_area_struct *vma,
 	if (mas_preallocate(mas, GFP_KERNEL))
 		goto nomem;
 
-	vma_start_write(vma);
+	vma_write_lock(vma);
 	vma_adjust_trans_huge(vma, start, end, 0);
 
 	if (file) {
@@ -634,7 +634,7 @@ inline int vma_expand(struct ma_state *mas, struct vm_area_struct *vma,
 	}
 
 	if (remove_next) {
-		vma_start_write(next);
+		vma_write_lock(next);
 		if (file) {
 			uprobe_munmap(next, next->vm_start, next->vm_end);
 			fput(file);
@@ -684,9 +684,9 @@ int __vma_adjust(struct vm_area_struct *vma, unsigned long start,
 	MA_STATE(mas, &mm->mm_mt, 0, 0);
 	struct vm_area_struct *exporter = NULL, *importer = NULL;
 
-	vma_start_write(vma);
+	vma_write_lock(vma);
 	if (next)
-		vma_start_write(next);
+		vma_write_lock(next);
 
 	if (next && !insert) {
 		if (end >= next->vm_end) {
@@ -734,7 +734,7 @@ int __vma_adjust(struct vm_area_struct *vma, unsigned long start,
 			if (remove_next == 2 && !next->anon_vma) {
 				exporter = next_next;
 				if (exporter)
-					vma_start_write(exporter);
+					vma_write_lock(exporter);
 			}
 
 		} else if (end > next->vm_start) {
@@ -889,7 +889,7 @@ int __vma_adjust(struct vm_area_struct *vma, unsigned long start,
 
 	if (remove_next) {
 again:
-		vma_mark_detached(next, true);
+		vma_mark_detached(vp->remove, true);
 		if (file) {
 			uprobe_munmap(next, next->vm_start, next->vm_end);
 			fput(file);
@@ -910,7 +910,7 @@ again:
 			remove_next = 1;
 			next = next_next;
 			if (next)
-				vma_start_write(next);
+				vma_write_lock(next);
 			goto again;
 		}
 	}
@@ -2391,7 +2391,7 @@ do_mas_align_munmap(struct ma_state *mas, struct vm_area_struct *vma,
 	int count = 0;
 	int error = -ENOMEM;
 	MA_STATE(mas_detach, &mt_detach, 0, 0);
-	mt_init_flags(&mt_detach, mas->tree->ma_flags &
+	mt_init_flags(&mt_detach, vmi->mas.tree->ma_flags &
 		      (MT_FLAGS_LOCK_MASK | MT_FLAGS_USE_RCU));
 	mt_set_external_lock(&mt_detach, &mm->mmap_lock);
 
