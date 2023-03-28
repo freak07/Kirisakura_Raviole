3feab10762840fb04b687406961666f02560ea4f
freak07 <michalechner92@googlemail.com>
2023-03-28 19:52:16 +0200

    Revert "mm: replace VM_LOCKED_CLEAR_MASK with VM_LOCKED_MASK"
    This reverts commit 11d33b80bb76793ce4b1f2ccb141584a3cacc2fc.
    
    (cherry picked from commit a775fc5c8a84670ba60d92b15d9a479b90ab2b0f)
    (cherry picked from commit 9b807c87e6fa146b49df41915c2c2b2ee8acaab5)
    (cherry picked from commit cf398a025b7dbc71330608f03694fb5a7c9bbef7)

--- mm/mremap.c
+++ mm/mremap.c
@@ -688,7 +688,7 @@ static unsigned long move_vma(struct vm_area_struct *vma,
 
 	if (unlikely(!err && (flags & MREMAP_DONTUNMAP))) {
 		/* We always clear VM_LOCKED[ONFAULT] on the old vma */
-		vm_flags_clear(vma, VM_LOCKED_MASK);
+		vma->vm_flags &= VM_LOCKED_CLEAR_MASK;
 
 		/* Because we won't unmap we don't need to touch locked_vm */
 		return new_addr;
