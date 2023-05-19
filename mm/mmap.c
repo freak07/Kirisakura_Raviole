8521a3a84507addc80e811585050b599ed84fabd
freak07 <michalechner92@googlemail.com>
2023-05-19 20:08:25 +0200

    Revert "mm: avoid rewalk in mmap_region"
    This reverts commit 6660f7f05947e9cddbd9ef8d98b179d0ae4995bc.
    
    (cherry picked from commit 920e4f196c2ff4fff6d79dcf81c07a3cf32694a1)

--- mm/mmap.c
+++ mm/mmap.c
@@ -3087,9 +3087,6 @@ unsigned long mmap_region(struct file *file, unsigned long addr,
 	}
 
 cannot_expand:
-	if (prev)
-		vma_iter_next_range(&vmi);
-
 	/*
 	 * Determine the object being mapped and call the appropriate
 	 * specific mapper. the address has already been validated, but
