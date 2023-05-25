a0282f61aaa7d8838b051175f554ab70aff485e9
freak07 <michalechner92@googlemail.com>
2023-05-25 21:05:43 +0200

    Revert "ANDROID: mm: do not allow file-backed pages from CMA"
    This reverts commit 3d83e0911534c6ef29d8aaff44e64f202d379c90.

--- mm/compaction.c
+++ mm/compaction.c
@@ -1098,8 +1098,6 @@ isolate_success:
 		list_add(&page->lru, &cc->migratepages);
 isolate_success_no_list:
 		cc->nr_migratepages += compound_nr(page);
-		if (!PageAnon(page))
-			cc->nr_migrate_file_pages += compound_nr(page);
 		nr_isolated += compound_nr(page);
 
 		/*
@@ -1140,7 +1138,6 @@ isolate_fail:
 			}
 			putback_movable_pages(&cc->migratepages);
 			cc->nr_migratepages = 0;
-			cc->nr_migrate_file_pages = 0;
 			nr_isolated = 0;
 		}
 
@@ -1287,14 +1284,9 @@ static bool suitable_migration_target(struct compact_control *cc,
 	if (cc->ignore_block_suitable)
 		return true;
 
-	if (cc->nr_migrate_file_pages) {
-		if (get_pageblock_migratetype(page) == MIGRATE_MOVABLE)
-			return true;
-	} else {
-		/* If the block is MIGRATE_MOVABLE or MIGRATE_CMA, allow migration */
-		if (is_migrate_movable(get_pageblock_migratetype(page)))
-			return true;
-	}
+	/* If the block is MIGRATE_MOVABLE or MIGRATE_CMA, allow migration */
+	if (is_migrate_movable(get_pageblock_migratetype(page)))
+		return true;
 
 	/* Otherwise skip the block */
 	return false;
@@ -2316,7 +2308,6 @@ compact_zone(struct compact_control *cc, struct capture_control *capc)
 	cc->total_migrate_scanned = 0;
 	cc->total_free_scanned = 0;
 	cc->nr_migratepages = 0;
-	cc->nr_migrate_file_pages = 0;
 	cc->nr_freepages = 0;
 	INIT_LIST_HEAD(&cc->freepages);
 	INIT_LIST_HEAD(&cc->migratepages);
@@ -2408,7 +2399,6 @@ compact_zone(struct compact_control *cc, struct capture_control *capc)
 			ret = COMPACT_CONTENDED;
 			putback_movable_pages(&cc->migratepages);
 			cc->nr_migratepages = 0;
-			cc->nr_migrate_file_pages = 0;
 			goto out;
 		case ISOLATE_NONE:
 			if (update_cached) {
@@ -2436,7 +2426,6 @@ compact_zone(struct compact_control *cc, struct capture_control *capc)
 
 		/* All pages were either migrated or will be released */
 		cc->nr_migratepages = 0;
-		cc->nr_migrate_file_pages = 0;
 		if (err) {
 			putback_movable_pages(&cc->migratepages);
 			/*
