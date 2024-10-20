550e3d050db3c96785fa96f2eea61c75a3149d44
freak07 <michalechner92@googlemail.com>
2024-10-20 06:59:15 +0200

    Merge tag 'v5.10.225' into up1a.231005_linux-stable_reset
    This is the 5.10.225 stable release

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -74,6 +74,10 @@ struct linux_binprm {
 #define BINPRM_FLAGS_PATH_INACCESSIBLE_BIT 2
 #define BINPRM_FLAGS_PATH_INACCESSIBLE (1 << BINPRM_FLAGS_PATH_INACCESSIBLE_BIT)
 
+/* preserve argv0 for the interpreter  */
+#define BINPRM_FLAGS_PRESERVE_ARGV0_BIT 3
+#define BINPRM_FLAGS_PRESERVE_ARGV0 (1 << BINPRM_FLAGS_PRESERVE_ARGV0_BIT)
+
 
 /*
  * This structure defines the functions that are used to load the binary formats that
