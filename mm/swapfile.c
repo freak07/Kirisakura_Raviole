b7743ded7142573dd7011164b4d8463ca971dcda
freak07 <michalechner92@googlemail.com>
2024-11-07 13:21:17 +0100

    Merge tag 'v5.10.228' into up1a.231005_linux-stable_reset
    This is the 5.10.228 stable release

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2136,7 +2136,7 @@ static int unuse_mm(struct mm_struct *mm, unsigned int type,
 
 	mmap_read_lock(mm);
 	for_each_vma(vmi, vma) {
-		if (vma->anon_vma) {
+		if (vma->anon_vma && !is_vm_hugetlb_page(vma)) {
 			ret = unuse_vma(vma, type, frontswap,
 					fs_pages_to_unuse);
 			if (ret)
