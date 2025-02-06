0693206ee295352c4b8a298527f33e7b75712f44
freak07 <michalechner92@googlemail.com>
2025-02-06 09:06:31 +0100

    Merge tag 'v5.10.229' into up1a.231005_linux-stable_reset
    This is the 5.10.229 stable release

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3192,6 +3192,8 @@ struct vm_area_struct *find_extend_vma_locked(struct mm_struct *,
 		unsigned long addr);
 int remap_pfn_range(struct vm_area_struct *, unsigned long addr,
 			unsigned long pfn, unsigned long size, pgprot_t);
+int remap_pfn_range_notrack(struct vm_area_struct *vma, unsigned long addr,
+		unsigned long pfn, unsigned long size, pgprot_t prot);
 int vm_insert_page(struct vm_area_struct *, unsigned long addr, struct page *);
 int vm_insert_pages(struct vm_area_struct *vma, unsigned long addr,
 			struct page **pages, unsigned long *num);
